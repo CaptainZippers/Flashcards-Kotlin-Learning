021c6337bc09fbe4eb6fe40f2333882a73e1a2fb
Nigel Krajcer <nphilmont2010@gmail.com>
2020-10-09 01:39:02 -0400

    Change cards to be self service

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -4,6 +4,19 @@ import java.util.*
 
 fun main() {
     val input = Scanner(System.`in`)
-    input.nextLine()
-    print("Your answer is ${if (input.nextLine() == input.nextLine())"right!" else "wrong..."}")
+    println("Input the number of cards:")
+    val numberOfCards = input.nextLine().toInt()
+    class Cards(val term:String, val definition: String)
+    val cards = arrayListOf<Cards>()
+    for (i in 1..numberOfCards) {
+        println("Card #$i:")
+        val term = input.nextLine()
+        println("The definition for card #$i:")
+        val definition = input.nextLine()
+        cards.add(Cards(term,definition))
+    }
+    for (card in cards) {
+        println("Print the definition of \"${card.term}\":")
+        println(if (input.nextLine() == card.definition) "Correct" else "Wrong. The right answer is \"${card.definition}\"")
+    }
 }
