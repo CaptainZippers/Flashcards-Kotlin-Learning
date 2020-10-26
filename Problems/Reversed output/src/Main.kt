483a632a51eb74d39db4db6cc415b4bc5cfcb35f
Nigel Krajcer <nphilmont2010@gmail.com>
2020-10-25 23:48:06 -0400

    Completed problem of the day regarding singletons

---
+++ Problems/Reversed output/src/Main.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() {
+    // put your code here
+    val first = readLine()!!
+    val second = readLine()!!
+    println(second)
+    print(first)
+}
---
+++ Problems/Rickrolling/src/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // put your code here
+    val one = readLine()!!
+    val two = readLine()!!
+    val three = readLine()!!
+    val four = readLine()!!
+    val five = readLine()!!
+    print(one + " ")
+    print(two + " ")
+    print(three + " ")
+    print(four + " ")
+    print(five)
+}
