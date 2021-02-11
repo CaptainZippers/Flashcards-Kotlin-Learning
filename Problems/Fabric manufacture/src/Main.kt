cffe826885ba8d37f839b005c32cf106c9c07b0d
Nigel Krajcer <nphilmont2010@gmail.com>
2021-02-10 21:35:22 -0500

    Secondary constructor problem solutions

---
+++ Problems/Fabric manufacture/src/Main.kt
@@ -0,0 +1,15 @@
+class Fabric(var color: String = "grey") {
+    var pattern: String = "none"
+    var patternColor: String = "none"
+    init {
+        println("$color fabric is created")
+    }
+
+    constructor(color: String, pattern: String,  patternColor: String): this() {
+        this.color = color
+        println("the fabric is dyed $color")
+        this.pattern = pattern
+        this.patternColor = patternColor
+        println("$patternColor $pattern pattern is printed on the fabric")
+    }
+}
